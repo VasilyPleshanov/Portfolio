7f43f35e5d1c5a27b6698cd8575f2f247290c8f2
Vasily Pleshanov <80349342+VasilyPleshanov@users.noreply.github.com>
2022-07-05 22:30:50 +0300

    Delete js directory

--- js/headerMenu.js
+++
@@ -1,9 +0,0 @@
-
-const burger = document.querySelector('.burger')
-const menu = document.querySelector('.menu')
-
-burger.addEventListener('click', () => {
-    menu.classList.toggle('menu_active')
-    burger.classList.toggle('burger_active')
-    document.body.classList.toggle('_lock')
-})
